60df622a453b992f3a410ea6dd9a769c5c304dc4
Igor Teixeira <43607581+iast94@users.noreply.github.com>
2024-01-04 14:58:05 -0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,35 @@
 # Configure the Provider
+
+##Digitalocean Provider
 terraform {
   required_providers {
-    <your-provider> = {
-      source  = "<your-provider>/<your-provider>"
-      version = "~> <your-provider-version>"
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
     }
   }
 }
+
+##AWS Provider
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 5.0"
+#    }
+#  }
+#}
+
+##GCP Provider
+#terraform {
+#  required_providers {
+#    google = {
+#      source = "hashicorp/google"
+#      version = "4.51.0"
+#    }
+#  }
+#}
+
 provider "<your-provider>" {
   token = var.do_token
 }
