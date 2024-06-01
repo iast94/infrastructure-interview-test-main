b6f23f579659da4753ed22ce19ee9b11f9eea046
Igor Teixeira <43607581+iast94@users.noreply.github.com>
2024-05-31 22:09:47 -0300

    Update image-build.sh

--- image-build.sh
+++ image-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build the Docker image locally
-docker build -t <username>/app:latest .
+docker build -t <username>/<image-name>:v1 .
 
 # Push the Docker image to DockerHub
-docker push <username>/app:latest
+docker push <username>/<image-name>:v1
